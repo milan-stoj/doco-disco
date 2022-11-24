cc5fef71724144265b892b19f8b5d02d751a0847
Milan <milanstoj94@gmail.com>
2022-11-24 14:41:46 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// var navLinks = document.getElementById("navLinks")
+// function showMenu(){
+//     navLinks.style.right = "0";
+// }
+// function hideMenu(){
+//     navLinks.style.right = "-200px";
+// }
+
+
+
+$( document ).ready(function() {
+    
+    // Function to change the nav-bar on scroll
+    // $(window).scroll(function(){
+    //     ($(window).scrollTop() >= 100) ? (
+    //         $('.fixed-nav-bar').addClass('scrolled'),
+    //         $('.the-bass').addClass('scrolled')
+    //     ) : (
+    //         $('.fixed-nav-bar').removeClass('scrolled'),
+    //         $('.the-bass').removeClass('scrolled')
+    //     );
+    // });
+    
+    // Drop Down Function
+    $('#menuButton').on('change', function(){
+        ($('#menuButton').is(':checked')) ? (
+            $('#navLinks').css({'right': '0px'})
+        ) : (
+            $('#navLinks').css({'right':'-200px'})
+        );
+    });
+
+    AOS.init();
+    
+});
---
+++ notes.md
@@ -0,0 +1,14 @@
+# Operation: Disco
+
+## Notes
+
+- City vibe
+- High-quality Images
+- Website dis.co
+    - Splash: Doco Disco
+    - People jamming out to different colors.
+    - Booking. Up-front Pricing.   
+
+
+- Mad Planet
+    - Partnership... not hiring us.
