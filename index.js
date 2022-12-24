e5a87b1b27269365358e2c58e4fb7b097c4e2502
Milan Stojanovic <milanstoj94@gmail.com>
2022-12-24 16:08:44 -0600

    fixed js

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-// var navLinks = document.getElementById("navLinks")
-// function showMenu(){
-//     navLinks.style.right = "0";
-// }
-// function hideMenu(){
-//     navLinks.style.right = "-200px";
-// }
-
-AOS.init();
-
 $("#nav-placeholder").load("nav.html");
 
 $( document ).ready(function() {
 
-    // Function to change the nav-bar on scroll
-    // $(window).scroll(function(){
-    //     ($(window).scrollTop() >= 100) ? (
-    //         $('.fixed-nav-bar').addClass('scrolled'),
-    //         $('.the-bass').addClass('scrolled')
-    //     ) : (
-    //         $('.fixed-nav-bar').removeClass('scrolled'),
-    //         $('.the-bass').removeClass('scrolled')
-    //     );
-    // });
-    
     // Drop Down Function
     $('#menuButton').on('change', function(){
         ($('#menuButton').is(':checked')) ? (
@@ -31,4 +10,7 @@ $( document ).ready(function() {
             $('#navLinks').css({'right':'-200px'})
         );
     });
+
 });
+
+AOS.init();
